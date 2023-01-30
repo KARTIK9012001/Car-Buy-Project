a8acb977e2cfc2d7cca02f784dcf6df9e79d086c
KARTIK9012001 <kartikpundir063@gmail.com>
2023-01-30 17:59:33 +0530

    Second commit

---
+++ src/components/data/Data.js
@@ -0,0 +1,286 @@
+export const nav = [
+  {
+    text: "home",
+    path: "/",
+  },
+  {
+    text: "about",
+    path: "/about",
+  },
+  {
+    text: "services",
+    path: "/services",
+  },
+  {
+    text: "blog",
+    path: "/blog",
+  },
+  {
+    text: "pricing",
+    path: "/pricing",
+  },
+  {
+    text: "contact",
+    path: "/contact",
+  },
+]
+export const featured = [
+  {
+    cover: "images/hero/h1.jpg",
+    name: "Hatchback",
+    total: "43 Available",
+  },
+  {
+    cover: "images/hero/h2.jpg",
+    name: "Crossover",
+    total: "28 Available",
+  },
+  {
+    cover: "images/hero/h3.jpg",
+    name: "Roadster",
+    total: "30 Available",
+  },
+  {
+    cover: "images/hero/h4.jpg",
+    name: "Open Wheel",
+    total: "80 Available",
+  },
+  {
+    cover: "images/hero/h6.jpg",
+    name: "Off-Roader",
+    total: "18 Available",
+  },
+]
+export const list = [
+  {
+    id: 1,
+    cover: "images/list/p-1.jpg",
+    name: "Mazda RX-7",
+    location: "Canada Registered",
+    category: "For Sale",
+    price: "$3,700",
+    type: "Racing",
+  },
+  {
+    id: 2,
+    cover: "images/list/p-2.jpg",
+    name: "Mc-Laren",
+    location: "Los Angeles Registered",
+    category: "For Sale",
+    price: "$9,750",
+    type: "Sports",
+  },
+  {
+    id: 3,
+    cover: "images/list/p-7.jpg",
+    name: "Audi",
+    location: "New York Registered",
+    category: "For Sale",
+    price: "$5,860",
+    type: "Sports",
+  },
+  {
+    id: 4,
+    cover: "images/list/p-4.jpg",
+    name: "Corvette",
+    location: "Las-Vegas Registered",
+    category: "For Sale",
+    price: "$7,540",
+    type: "Racing",
+  },
+  {
+    id: 5,
+    cover: "images/list/p-5.jpg",
+    name: "Mustang",
+    location: "Canada Regsitered",
+    category: "For Sale",
+    price: "$4,850",
+    type: "American Muscle",
+  },
+  {
+    id: 6,
+    cover: "images/list/p-6.jpg",
+    name: "Range Rover",
+    location: "Las Vegas Registered",
+    category: "For Sale",
+    price: "$2,742",
+    type: "Premium SUV",
+  },
+]
+export const awards = [
+  {
+    icon: <i class='fa-solid fa-trophy'></i>,
+    num: "32 M	",
+    name: "Blue Burmin Award",
+  },
+  {
+    icon: <i class='fa-solid fa-briefcase'></i>,
+    num: "43 M",
+    name: "Mimo X11 Award",
+  },
+  {
+    icon: <i class='fa-solid fa-lightbulb'></i>,
+    num: "51 M",
+    name: "Australian UGC Award",
+  },
+  {
+    icon: <i class='fa-solid fa-heart'></i>,
+    num: "42 M",
+    name: "IITCA Green Award",
+  },
+]
+export const location = [
+  {
+    id: 1,
+    name: "Audi",
+    
+    cover: "images/location/city-1.jpg",
+  },
+  {
+    id: 2,
+    name: "Lamborghini",
+   
+    cover: "images/location/city-2.jpg",
+  },
+  {
+    id: 3,
+    name: "BMW",
+   
+    cover: "images/location/city-3.jpg",
+  },
+  {
+    id: 4,
+    name: "Ferrari",
+   
+    cover: "images/location/city-4.jpg",
+  },
+  {
+    id: 5,
+    name: "Mercedez",
+    
+    cover: "images/location/city-5.jpg",
+  },
+  {
+    id: 6,
+    name: "Bentley",
+    
+    cover: "images/location/city-6.jpg",
+  },
+]
+export const team = [
+  {
+    list: "50",
+    cover: "images/customer/team-1.jpg",
+    address: "Liverpool, Canada",
+    name: "Sargam S. Singh",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "70",
+    cover: "images/customer/team-2.jpg",
+    address: "Montreal, Canada",
+    name: "Harijeet M. Siller",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "80",
+    cover: "images/customer/team-3.jpg",
+    address: "Denever, USA",
+    name: "Anna K. Young",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  
+]
+export const price = [
+  {
+    plan: "Basic",
+    price: "29",
+    ptext: "per user, per month",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Car Consultancy",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Daily Updates",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Reviews",
+      },
+      { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "Insurance Offers" },
+      { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "Upcoming Launches" },
+    ],
+  },
+  {
+    best: "Best Value",
+    plan: "Standard",
+    price: "49",
+    ptext: "per user, per month",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Car Consultancy",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Daily Updates",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Reviews",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Insurance Offers",
+      },
+      {
+        change: "color",
+        icon: <i class='fa-solid fa-x'></i>,
+        text: "Upcoming Launches",
+      },
+    ],
+  },
+  {
+    plan: "Platinum",
+    price: "79",
+    ptext: "2 user, per month",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Car Consultancy",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Daily Updates",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Reviews",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Insurance Offers",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Upcoming Launches",
+      },
+    ],
+  },
+]
+export const footer = [
+  {
+    title: "LAYOUTS",
+    text: [{ list: "Home Page" }, { list: "About Page" }, { list: "Service Page" },, { list: "Contact Page" }],
+  },
+  {
+    title: "ALL SECTIONS",
+    text: [{ list: "Features" }, { list: "Videos" }, { list: "Footers" },{ list: "Updates" }],
+  },
+  {
+    title: "COMPANY",
+    text: [{ list: "About" }, { list: "Blog" }, { list: "Pricing" }, { list: "Login" }],
+  },
+]
